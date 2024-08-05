f3acedc3867509b66c9d7c97a2df287186c88b0b
Samm Wiltshire <samm94tw@gmail.com>
2024-08-05 14:42:24 +0100

    string

--- variables.js
+++ variables.js
@@ -1,7 +1,17 @@
-console.log("All around the wolrd".charAt(7).toLocaleUpperCase())
+// console.log("All around the wolrd".charAt(7).toLocaleUpperCase())
 
-let my_ier = 10;
+// let my_ier = 10;
 
 // i = i % 5;
 
-console.log(i)
+// console.log(i)
+
+// let FavouriteDrink = "ProteinShake"
+
+// console.log("my favourite drink is " + FavouriteDrink);
+
+let name = "Samm";
+let age = 29;
+let FavouriteDrink = "{ProteinShake"
+
+console.log("Hi. My name is " + name + ". My age is " + age + " and my fav drink is " + FavouriteDrink)
