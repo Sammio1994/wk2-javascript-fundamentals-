a3ff60092912b6f0f779b2252ca2420c2513e44e
Samm Wiltshire <samm94tw@gmail.com>
2024-08-05 12:20:45 +0100

    challenge

--- dotnotation.js
+++ dotnotation.js
@@ -1,3 +1,15 @@
 console.log("The War Of The Stars Has Started".toUpperCase())
 
 console.log(Math.floor( Math.random()*10) )
+
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")    
+console.log("  |   |  ")
