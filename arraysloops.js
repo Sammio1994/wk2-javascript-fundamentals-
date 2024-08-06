e3761605d96a01e7ccca71fbcd9933320a5b90f0
Samm Wiltshire <samm94tw@gmail.com>
2024-08-06 14:33:53 +0100

    arrayssplice

--- arraysloops.js
+++ arraysloops.js
@@ -1,9 +1,19 @@
-let coffeeOrder = [
-    "Alex - Cortado",
-    "Ben - Capa",
-    "Charlie - Whatsevers new"
+// let coffeeOrder = [
+//     "Alex - Cortado",
+//     "Ben - Capa",
+//     "Charlie - Whatsevers new"
+// ]
+// coffeeOrder.pop();
+
+// console.log (coffeeOrder);
+
+let favouriteSongs = [
+    "somethine - something ",
+    "something - something",
+    "something - something"
 ]
-coffeeOrder.pop();
+favouriteSongs.pop();
 
-console.log (coffeeOrder);
-
+console.log (favouriteSongs);
+favouriteSongs.splice(2, 0, "something, something")
+console.log(favouriteSongs[1]);
