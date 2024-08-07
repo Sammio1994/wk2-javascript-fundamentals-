c75d7897e2df0c64082ff697755e00b4c605469c
Samm Wiltshire <samm94tw@gmail.com>
2024-08-07 10:41:00 +0100

    functions

--- functions.js
+++ functions.js
@@ -1,9 +1,18 @@
-let age = 18;
+// let age = Math.floor(Math.random()*90)
 
-if (age < 18) {
-    console.log(`Child ticket`)
-} else if (18 <= age && age < 60) {
-    console.log(`Adult ticket`) 
-} else { 
-    console.log(`Senior`)
+// if (age < 18) {
+//     console.log(`Child ticket`)
+// } else if (18 <= age && age < 60) {
+//     console.log(`Adult ticket`) 
+// } else { 
+//     console.log(`Senior`)
+// }
+
+const pressGrindBeans = () => {
+    console.log(`Gridining for 20 seconds`);
 }
+
+pressGrindBeans();
+pressGrindBeans();
+pressGrindBeans();
+pressGrindBeans();
