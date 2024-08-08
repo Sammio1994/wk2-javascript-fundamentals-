0c89207b49c73b945603cbf9a293cd125ea73b3c
Samm Wiltshire <samm94tw@gmail.com>
2024-08-08 15:28:48 +0100

    Act3

--- objects.js
+++ objects.js
@@ -29,16 +29,26 @@
 // pet.eat = ("fruit")
 // console.log(pet.sayHi())
 
-// // Act 3
-// let coffeeShop = {
-//     branch: "BigWhite",
-//     menu: {food: ["Carbonarra £3.00, Lasania £2.50"
-//     ], drink: ["Coffee £4.00, Wine £6.00"]},
-//     Order(){
+// Act 3
+let coffeeShop = {
+    branch: "BigWhite",
+    menu: {food: ["Carbonarra", 3.00, "Lasania", 2.50
+    ], drink: ["Coffee", 4.00, "Wine", 6.00]},
+    foodOne: "Carbonarra",
+    drinkOne: "Wine",
+    foodTwo: "Lasania",
+
+    Order(){    console.log(`Welcome to ${this.branch} coffee shop, what would you like?
+      ,I will demolish One ${this.foodOne} and quench my thirst with Two ${this.drinkOne}`)
+      
+    let total = 0
+    total += this.menu.food[this.menu.food.indexOf(this.foodOne)+1]
+    total += this.menu.drink[this.menu.drink.indexOf(this.drinkOne)+1]
+    console.log(`That will come too ${total}`)
+    }
         
-//         return `One ${food}, Two ${drink}`
-        
-//     }       
-// }
-// console.log(coffeeShop.menu.food)
-// console.log(coffeeShop.menu.drink)
+
+// let orderTotal{(`Your' order comes too ${price} how will you be paying?`)
+    }       
+coffeeShop.Order()
+
