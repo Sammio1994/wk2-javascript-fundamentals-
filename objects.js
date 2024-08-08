bc26ff9d13f35225d35217986e093670980926eb
Samm Wiltshire <samm94tw@gmail.com>
2024-08-08 11:50:29 +0100

    Act2

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
-let newPerson = {
-    name: "Effy",
-    age: 33,
-    sayHi(){
-        console.log(this.name)
-        return `I am ${this.name}`
-    }
-}
-console.log(newPerson.sayHi())
+// let newPerson = {
+//     name: "Effy",
+//     age: 33,
+//     sayHi(){
+//         console.log(this.name)
+//         return `I am ${this.name}`
+//     }
+// }
+// console.log(newPerson.sayHi())
 
 // console.log(person.age)
 // let chosenKey = "name"
@@ -15,3 +15,16 @@ console.log(newPerson.sayHi())
 // person.favSongs = ["we will rock you"]
 
 // console.log (person)
+
+// Act2
+// let pet = {
+//     name: "Nova and Kenneth",
+//     age: 1 & 2,
+//     color: "black",
+//     sayHi(){
+//         return `My dogs are called ${this.name} they will${this.eat}`
+//     }
+//     }
+
+// pet.eat = ("fruit")
+// console.log(pet.sayHi())
